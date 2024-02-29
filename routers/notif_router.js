f74d1c0763efe96decaa61e4f31f933adae69e9c
Sadat Hossain <sadatho2014@gmail.com>
2024-03-01 00:01:55 +0600

    added notifications API

--- routers/notif_router.js
+++ routers/notif_router.js
@@ -46,4 +46,78 @@ router.get("/retrieve", async (req, res) => {
   }
 });
 
+router.post("/mark-as-read", async (req, res) => {
+  const { data, error } = await get_user(req);
+  if (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ error });
+    return;
+  }
+
+  const user_id = data.user.id;
+  const { notification_id } = req.body;
+
+  console.log("notification_id: ", notification_id);
+
+  let query;
+
+  if (notification_id == -1) {
+    // Mark all notifications as read
+    query = `
+      UPDATE
+        "UserNotification"
+      SET
+        is_checked = TRUE
+      WHERE
+        user_id = $1
+    `;
+  } else {
+    query = `
+      UPDATE
+        "UserNotification"
+      SET
+        is_checked = TRUE
+      WHERE
+        user_id = $1 AND
+        notification_id = $2
+  `;
+  }
+
+  try {
+    await db.none(query, [user_id, notification_id]);
+    res.status(200).json({ message: "Notification marked as read" });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.delete("/delete", async (req, res) => {
+  const { data, error } = await get_user(req);
+  if (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ error });
+    return;
+  }
+
+  const user_id = data.user.id;
+  const { notification_id } = req.body;
+
+  const query = `
+    DELETE FROM
+      "UserNotification"
+    WHERE
+      user_id = $1 AND
+      notification_id = $2
+  `;
+
+  try {
+    await db.none(query, [user_id, notification_id]);
+    res.status(200).json({ message: "Notification deleted" });
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 export default router;
